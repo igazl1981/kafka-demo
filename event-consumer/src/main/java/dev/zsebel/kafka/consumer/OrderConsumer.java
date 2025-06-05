a46067c2c171c3a84071332a74076188817c1e81
Zsolt Ebel <ebelzsolt@gmail.com>
2025-06-05 21:38:01 +0200

    Fix SonarQube code smell

--- event-consumer/src/main/java/dev/zsebel/kafka/consumer/OrderConsumer.java
+++ event-consumer/src/main/java/dev/zsebel/kafka/consumer/OrderConsumer.java
@@ -14,12 +14,12 @@ public class OrderConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
 
     @KafkaListener(topics = "orders", containerFactory = "kafkaListenerContainerFactory")
-    public void consume(ConsumerRecord<Long, Order> record) {
-        Order order = record.value();
+    public void consume(ConsumerRecord<Long, Order> consumerRecord) {
+        Order order = consumerRecord.value();
         LOGGER.info(
             "Received event - topic: {}, partition: {}, orderId: {}, productCount: {}",
-            record.topic(),
-            record.partition(),
+            consumerRecord.topic(),
+            consumerRecord.partition(),
             order.orderId(),
             order.products().size()
         );
